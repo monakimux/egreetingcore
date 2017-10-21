6b493fde9019092e896c48037689481a3c453982
monakimux <monakimux@gmail.com>
2017-10-21 16:28:22 -0700

    initial commit

---
+++ halloween/Model/Greetings.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace halloween.Model
+{
+    public class Greetings
+    {
+        [DisplayName("Your name")]
+        [Display(Prompt = "Your name")]
+        [Required(ErrorMessage = "Required")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "You must enter between 3 to 100 characters")]
+        public string fromName { get; set; }
+
+        [DisplayName("Your email")]
+        [Display(Prompt = "username@domain.com")]
+        [Required(ErrorMessage = "Required")]
+        public string fromEmail { get; set; }
+
+        [DisplayName("Your friend's name")]
+        [Display(Prompt = "Your friend's name")]
+        [Required(ErrorMessage = "Required")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "You must enter between 3 to 100 characters")]
+        public string toName { get; set; }
+
+        [DisplayName("Your friend's email")]
+        [Display(Prompt = "username@domain.com")]
+        [Required(ErrorMessage = "Required")]
+        public string toEmail { get; set; }
+
+        [DisplayName("Subject")]
+        [Required(ErrorMessage = "Required")]
+        public string subject { get; set; }
+
+        [DisplayName("Personalized Message")]
+        [Required(ErrorMessage = "Required")]
+        public string message { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string tandcagree { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string createDate { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string createIP { get; set; }
+
+    }
+
+}
---
+++ halloween/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using halloween.Model;
+
+namespace halloween.Pages
+{
+    public class IndexModel : PageModel
+    {
+        // DEFAULT MODE
+        public void OnGet()
+        {
+            isPreviewPage = false;
+        }
+
+        // PREVIEW MODE (AFTER SUBMITTING)
+        public void OnPost()
+        {
+            isPreviewPage = true;
+            
+        }
+
+        // BRIDGE TO GREETINGS MODEL
+        [BindProperty]
+        public Greetings bridgeGreetings { get; set; }
+           
+        // TEST IF USER IS LOOKING AT PREVIEW OR FORM
+        public bool isPreviewPage { get; set; }
+
+
+
+    }
+}
