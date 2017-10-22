7bb40d815612c6b75fe503d825af00a943792092
monakimux <monakimux@gmail.com>
2017-10-21 18:36:31 -0700

    playing with the css

--- halloween/Model/Greetings.cs
+++ halloween/Model/Greetings.cs
@@ -11,38 +11,38 @@ namespace halloween.Model
     {
         [DisplayName("Your name")]
         [Display(Prompt = "Your name")]
-        [Required(ErrorMessage = "Required")]
+        [Required(ErrorMessage = "Your name is required")]
         [StringLength(100, MinimumLength = 3, ErrorMessage = "You must enter between 3 to 100 characters")]
         public string fromName { get; set; }
 
         [DisplayName("Your email")]
         [Display(Prompt = "username@domain.com")]
-        [Required(ErrorMessage = "Required")]
+        [Required(ErrorMessage = "Your email is required")]
         public string fromEmail { get; set; }
 
         [DisplayName("Your friend's name")]
         [Display(Prompt = "Your friend's name")]
-        [Required(ErrorMessage = "Required")]
+        [Required(ErrorMessage = "Your friend's name is required")]
         [StringLength(100, MinimumLength = 3, ErrorMessage = "You must enter between 3 to 100 characters")]
         public string toName { get; set; }
 
         [DisplayName("Your friend's email")]
         [Display(Prompt = "username@domain.com")]
-        [Required(ErrorMessage = "Required")]
+        [Required(ErrorMessage = "Your friend's email is required")]
         public string toEmail { get; set; }
 
         [DisplayName("Subject")]
         [Display(Prompt = "Subject")]
-        [Required(ErrorMessage = "Required")]
+        [Required(ErrorMessage = "Subject Required")]
         public string subject { get; set; }
 
         [DisplayName("Message")]
         [Display(Prompt = "Your message")]
-        [Required(ErrorMessage = "Required")]
+        [Required(ErrorMessage = "Message Required")]
         public string message { get; set; }
 
-        [Required(ErrorMessage = "Required")]
-        public string tandcagree { get; set; }
+        [Required(ErrorMessage = "You must agree to our terms and conditions")]
+        public bool tandcagree { get; set; }
 
         [Required(ErrorMessage = "Required")]
         public string createDate { get; set; }
